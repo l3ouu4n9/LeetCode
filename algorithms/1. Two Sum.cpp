6f9a97af307d3e2f4d6dc116eae1460f131e473d
Leo Wang <star901799@gmail.com>
2019-12-03 16:24:23 +0800

    Create 1. Two Sum.cpp

---
+++ algorithms/1. Two Sum.cpp
@@ -0,0 +1,31 @@
+/*
+  Given nums = [2, 7, 11, 15], target = 9,
+
+  Because nums[0] + nums[1] = 2 + 7 = 9,
+  return [0, 1].
+  
+  
+  
+  Runtime: 13.46%
+  Memory: 97.82%
+*/
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); ++i){
+            int tmp = target - nums[i];
+            for(int j = i + 1; j < nums.size(); ++j){
+                if(nums[j] == tmp){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    i = nums.size();
+                    j = nums.size();
+                }
+            }
+        }
+        return ans;
+    }
+};
