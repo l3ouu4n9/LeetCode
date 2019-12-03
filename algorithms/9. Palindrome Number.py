b6292cfc6d5185f43fef940d9acc05b2cb6b66af
Leo Wang <star901799@gmail.com>
2019-12-03 16:36:33 +0800

    Create 9. Palindrome Number.py

---
+++ algorithms/9. Palindrome Number.py
@@ -0,0 +1,34 @@
+'''
+
+Determine whether an integer is a palindrome.
+An integer is a palindrome when it reads the same backward as forward.
+
+E.g.
+  Input: 121
+  Output: true
+  
+  Input: -121
+  Output: false
+  Explanation: From left to right, it reads -121. 
+  From right to left, it becomes 121-.
+  
+  Input: 10
+  Output: false
+  Explanation: Reads 01 from right to left.
+  
+  Runtime: 30.55%
+  Memory: 45.78%
+
+'''
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s = str(x)
+        if(s == s[::-1]):
+            return True
+        else:
+            return False
