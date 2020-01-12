643d3411a5b2c1ac7b1fadd4de0fd7de1945410a
Leo Wang <star901799@gmail.com>
2020-01-12 11:00:36 +0800

    Create 232. Implement Queue using Stacks.cpp

---
+++ algorithms/232. Implement Queue using Stacks.cpp
@@ -0,0 +1,68 @@
+/*
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);  
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+
+Notes:
+
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+*/
+
+class MyQueue {
+private:
+    stack<int> s;
+    stack<int> tmp;
+public:
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        while(!s.empty()){
+            tmp.push(s.top());
+            s.pop();
+        }
+        s.push(x);
+        while(!tmp.empty()){
+            s.push(tmp.top());
+            tmp.pop();
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int value = s.top();
+        s.pop();
+        return value;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        return s.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
