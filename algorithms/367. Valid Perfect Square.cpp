1ad38061659576e3b5b5db8a36bd2b7fc59c5ac0
Leo Wang <star901799@gmail.com>
2020-01-23 07:04:06 +0800

    Create 367. Valid Perfect Square.cpp

---
+++ algorithms/367. Valid Perfect Square.cpp
@@ -0,0 +1,26 @@
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+
+Example 2:
+
+Input: 14
+Output: false
+*/
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long_num = long(num);
+        for(long i = 1; i * i <= num; ++i){
+            if(i * i == num) return true;
+        }
+        return false;
+    }
+};
