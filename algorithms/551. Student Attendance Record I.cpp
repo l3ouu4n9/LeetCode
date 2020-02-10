9fb5b611e546a899afaf9eb6d940ed72ac95ec2e
Leo Wang <star901799@gmail.com>
2020-02-10 08:54:25 +0800

    Create 551. Student Attendance Record I.cpp

---
+++ algorithms/551. Student Attendance Record I.cpp
@@ -0,0 +1,39 @@
+/*
+You are given a string representing an attendance record for a student. The record only contains the following three characters:
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+
+Input: "PPALLP"
+Output: True
+Example 2:
+
+Input: "PPALLL"
+Output: False
+*/
+
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int absent = 0;
+        int late = 0;
+        for(int i = 0; i < s.length(); ++i){
+            if(s[i] == 'L'){
+                ++late;
+                if(late == 3) return false;
+            } else {
+                if(s[i] == 'A'){
+                    ++absent;
+                    if(absent == 2) return false;
+                }
+                if(late != 0) late = 0;
+            }
+        }
+        return true;
+    }
+};
