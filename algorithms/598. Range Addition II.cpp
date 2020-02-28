7d97e8da5834d0de74d8b507a5bdc91abb43d9f3
Leo Wang <star901799@gmail.com>
2020-02-28 10:34:40 +0800

    Update 598. Range Addition II.cpp

--- algorithms/598. Range Addition II.cpp
+++ algorithms/598. Range Addition II.cpp
@@ -35,3 +35,14 @@ The range of m and n is [1,40000].
 The range of a is [1,m], and the range of b is [1,n].
 The range of operations size won't exceed 10,000.
 */
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        for(int i = 0 ; i < ops.size(); ++i){
+            m = min(ops[i][0], m);
+            n = min(ops[i][1], n);
+        }
+        return m * n;
+    }
+};
